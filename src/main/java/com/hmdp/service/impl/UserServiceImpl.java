65b57f9e912dd1d39b6436b68b0eed85759ce694
Your Name <you@example.com>
2024-10-27 20:56:38 +0800

    实现登录校验拦截器和隐藏用户敏感信息

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -43,9 +48,49 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 5.发送验证码
         String subject = "您的验证码";
         String text = "您的验证码是：" + code;
-        mailService.sendEmail("20224062@stu.neu.edu.cn", subject, text); // 替换为目标邮箱
+//        mailService.sendEmail("20224062@stu.neu.edu.cn", subject, text); // 替换为目标邮箱
         log.debug("发送短信验证码成功，验证码：{}",code);
         // 返回ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        // 2.如果有不符合，返回错误信息
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误！");
+        }
+        // 3.校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        // 4.不一致则报错
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误");
+        }
+
+        // 5.一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        // 6.判断用户是否存在
+        if (user == null) {
+            // 7.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        // 8.保存用户信息到session中
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone){
+        //1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        //2.保存用户
+        save(user);
+        return user;
     }
 }
